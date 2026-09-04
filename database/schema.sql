5138289c80b5ba6e2f1da8726131ff62186c8c54
Raj1778 <rajmhr078@gmail.com>
2026-09-04 23:56:02 +0530

    feat: implement show seats and refactor shows schema

--- database/schema.sql
+++ database/schema.sql
@@ -27,38 +27,49 @@ CREATE TABLE venues (
 CREATE TABLE events(
     id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     organizer_id INT NOT NULL REFERENCES organizers(id) ON DELETE CASCADE,
-    title VARCHAR(100),
+    title VARCHAR(100) NOT NULL,
     description VARCHAR(1000),
     category VARCHAR(50) NOT NULL DEFAULT 'general'
-        CHECK (category IN('movie', 'concert', 'standup', 'sports', 'theatre', 'general'))
+        CHECK (category IN('movie', 'concert', 'standup', 'sports', 'theatre', 'general')),
     duration_minutes INT,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
-)
+); --this could be a movie "Sholay" or a standup show "Jealous of Sabziwala" 
 
 
 CREATE TABLE shows (
     id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    organizer_id INT NOT NULL REFERENCES organizers(id) on DELETE CASCADE,
     venue_id INT NOT NULL REFERENCES venues(id),
+    event_id INT NOT NULL REFERENCES events(id),
     -- title VARCHAR (50) NOT NULL,
     -- description TEXT, 
     --earlier there was no "Events" table but due to data redundancy and inconsistency, we added an Events table
     --So, description and text is now a part of Event
     starts_at TIMESTAMPTZ NOT NULL,
     status VARCHAR(20) NOT NULL DEFAULT 'Coming soon...'
-        CHECK(status IN('       ', 'Published', 'Cancelled', 'Completed')),
+        CHECK(status IN('Coming soon...', 'Published', 'Cancelled', 'Completed')),
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
-);
+); --this means Sholay at PVR INOX Pune, or Jealous of Sabziwala Habitat
 
 CREATE TABLE seats(
     id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    show_id INT NOT NULL REFERENCES shows(id);
+    venue_id INT NOT NULL REFERENCES venues(id),
+    section VARCHAR(50) DEFAULT 'Standard'
+        CHECK(section IN('Standard', 'Premium', 'Balcony')),
+    row_label VARCHAR(10),             -- "A", "B", "C"... NULL for General Admission
+    seat_number INT,                   -- 1, 2, 3... NULL for General Admission
+    UNIQUE(venue_id, section, row_label, seat_number)
+);
 
-)
 
+CREATE TABLE show_seats (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    show_id INT NOT NULL REFERENCES shows(id) ON DELETE CASCADE,
+    seat_id INT REFERENCES seats(id),   
 
+    status VARCHAR(20) NOT NULL DEFAULT 'available'
+        CHECK (status IN ('available', 'booked')),
 
-
-
+    UNIQUE(show_id, seat_id)
+);
